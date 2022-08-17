cfa39e2ba66bc23f24b599759f8ac79c6f512ece
Felipe Ferraz <feliperodriguesferraz16@gmail.com>
2022-08-16 22:19:34 -0400

    Base do Projeto Completa

--- src/router.ts
+++ src/router.ts
@@ -1,8 +1,5 @@
 import { Router } from "express";
-import { exemploController} from "./controller/ExemploController";
 
 const router: Router = Router()
-
-router.get("/", exemploController.teste);
 
 export { router };
